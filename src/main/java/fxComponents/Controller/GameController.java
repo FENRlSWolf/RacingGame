cd3b493884d1b3a330d31e4cbc546d787005c20f
Honorabilis-I <niklas.n.kolb@web.de>
2026-01-13 14:15:34 +0100

    Foundation for Game Controller

---
+++ src/main/java/fxComponents/Controller/GameController.java
@@ -0,0 +1,10 @@
+package fxComponents.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
+// for Game Loop code
+public class GameController {
+    @FXML
+    Canvas gamePanel = new Canvas(512, 512);
+}
