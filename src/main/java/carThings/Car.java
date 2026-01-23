3cb7e18673ca1738c014e5c8d81d77553ff21587
Honorabilis-I <niklas.n.kolb@web.de>
2026-01-23 11:27:41 +0100

    car background removed (thanks melina :) )
    Car physics updated to vectors with grip to drift (

--- src/main/java/carThings/Car.java
+++ src/main/java/carThings/Car.java
@@ -1,105 +1,15 @@
 package carThings;
 
 import fxComponents.AssetManager;
+import helpers.Vector2;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Car extends GameObject {
-    //Linecoment just for testing
-
-    /*
-    //stats editable
-    double power; //influences acceleration
-    double handlingStat; //influences cornerperformance
-    double weight; //influences breakingPower, traction, acceleration
-    double breakingPower;
-
-
-
-
-    //stats that are based on other stats
-    double accelStat;
-    double traction;
-
-    //other stats
-    double dragInPercent = 5;
-
-
-
-    //others
-    PlayerInput playerInput = new PlayerInput();
-
-
-    Vector2 movementVector;
-
-    public Car(double x, double y) {
-        super(x, y);
-    }
-
-    @Override
-   public void update() {
-        super.update();
-        move(movementVector);
-    }
-    */
-
-    /**
-     * returns a MovementVector2 that is local to the cars rotation, thus the rotation has to somehow be account to
-     * @return Vector2
-     */
-    /*
-    Vector2 localTermMovementVector2() {
-        Vector2 localMovementVector = new Vector2(0, 0);
-        //do calculation here
-
-
-
-
-
-        //drag
-        localMovementVector.x -= (localMovementVector.x * dragInPercent);
-        if (localMovementVector.y <= 0) {
-            localMovementVector.x = 0;
-        }
-
-
-
-        if (playerInput.isHandBreaking()) {
-            //decrease traction and slow down
-
-
-        }
-        if(playerInput.isAccelerating()) {
-            localMovementVector.y += accelStat; //later add framedeltatime and traction
-        }
-        if (playerInput.isBreaking()) {
-            localMovementVector.y -= breakingPower; //later add framedeltatime and traction
-        }
-        if (playerInput.isGoingLeft()) {
-            localMovementVector.x -= handlingStat; //later add framedeltatime and traction
-        }
-        if (playerInput.isGoingRight()) {
-            localMovementVector.x += handlingStat; //later add framedeltatime and traction
-        }
-
-
-
-
-        return localMovementVector;
-    }
-
-
-
-    public void draw(GraphicsContext gc) {
-        //gc.drawImage(carImage, position.x, position.y);
-    }
-  */
 
     //connectors
     PlayerInput input;
 
-
     //Unit: Pixel per Second (PPS)
-    private double speed = 0;
     private double turnSpeed = 0;
 
     //Unit: percent
@@ -107,20 +17,28 @@ public class Car extends GameObject {
 
     //Unit: Degrees
     private double rotation = 0;
-    private double moveDirection = 0;
     //Unit: px/s²
     private static final double ACCEL = 200;
 
     //Unit: px/s
-    private static final double MAX_SPEED = 3200;
+    private static final double MAX_SPEED = 600;
 
     //Unit: dgr/s (degrees per second)
     private static final double TURN_ACCEL = 30; //was 180
     private static final double MAX_TURN_SPEED = 200;
-    private static final double MIN_TURN_SPEED = -MAX_TURN_SPEED;
 
     //unitless
-    private static final double DRAG = 0.98;
+    private static final double DRAG = 0.995;
+
+    //starting with vectors
+    //Vector for forward force
+    public Vector2 velocity = new Vector2(0,0);
+
+    //Vector for sideway force
+    public Vector2 lateralForce;
+
+    //Vector for pointing direction
+    public Vector2 forward = new Vector2(0,-1);
 
     public Car(double x, double y){
         super(x, y);
@@ -133,55 +51,63 @@ public class Car extends GameObject {
             //should be changed in the future if there are more sources to change the traction
             traction = 1;
         }
-        if (input.accelerate()) speed += ACCEL * dt;
-        if (input.brake()) speed -= ACCEL *dt;
+        if (input.accelerate()) {
+            Vector2 accelerationForce = new Vector2(
+                    forward.x * ACCEL,
+                    forward.y * ACCEL
+            );
 
+            velocity.x += accelerationForce.x * dt;
+            velocity.y += accelerationForce.y * dt;
+        }
+        if (input.brake()) {
+            Vector2 accelerationForce = new Vector2(
+                    forward.x * ACCEL,
+                    forward.y * ACCEL
+            );
 
-
-
-        //speed limitation
-        speed = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, speed));
-        speed *= DRAG;
-        int zeroVelThreshold = 10;
-        if(speed <= zeroVelThreshold && speed >= -zeroVelThreshold && !(input.accelerate() || input.brake())) {
-            speed = 0;
+            velocity.x -= accelerationForce.x * dt;
+            velocity.y -= accelerationForce.y * dt;
         }
 
 
+        velocity.x *= DRAG;
+        velocity.y *= DRAG;
 
-        double rad = Math.toRadians(moveDirection);
+//        int zeroVelThreshold = 10;
+//        if(speed <= zeroVelThreshold && speed >= -zeroVelThreshold && !(input.accelerate() || input.brake())) {
+//            speed = 0;
+//        }
 
-        position.x += Math.sin(rad) * speed * dt;
-        position.y -= Math.cos(rad) * speed *dt;
+        position.x +=velocity.x * dt;
+        position.y += velocity.y *dt;
     }
 
     private void steeringCalc(double dt) {
         int steering = input.steering();
         System.out.println(steering);
 
-        if(steering != 0) {
-            //car is turning
-            turnSpeed += TURN_ACCEL * speed/MAX_SPEED;
-            if(turnSpeed > MAX_TURN_SPEED) {
-                turnSpeed = MAX_TURN_SPEED;
-                //checking "reverse" turnspeed cap
-            } else if (turnSpeed < MIN_TURN_SPEED ) {
-                turnSpeed = MIN_TURN_SPEED;
-            }
-        } else {
-            turnSpeed = 0;
-        }
-        if (speed != 0) {
-            //car orientation
+        //steering
+        if (velocity.magnitude() != 0) {
+            double steeringStrengh = (velocity.magnitude()/ MAX_SPEED);
+            steeringStrengh = Math.min(1, steeringStrengh) * 3.5;
+
             if (steering == -1) {
-                rotation -= turnSpeed * dt;
-                moveDirection -= turnSpeed * dt * traction;
+                rotation -= TURN_ACCEL * steeringStrengh * dt;
             }
             if (steering == 1) {
-                rotation += turnSpeed * dt;
-                moveDirection = rotation + (turnSpeed * dt * traction);
+                rotation += TURN_ACCEL * steeringStrengh * dt;
             }
 
+            forward.x = Math.sin(Math.toRadians(rotation));
+            forward.y = -Math.cos(Math.toRadians(rotation));
+
+            //grip to 0.0001 for ice like driving
+            double grip = 1;
+            double speed = velocity.magnitude();
+
+            velocity.x = velocity.x * (1 - grip) + forward.x * speed * grip;
+            velocity.y = velocity.y * (1 - grip) + forward.y * speed * grip;
         }
     }
 
@@ -194,7 +120,7 @@ public class Car extends GameObject {
         gc.restore();
 
         //debug for showing car coordinates
-//        System.out.println("X: " + position.x + " Y: " + position.y);
-        System.out.println("Geschwindigkeit: " + speed);
+        System.out.println("X: " + position.x + " Y: " + position.y);
+        System.out.println("Geschwindigkeit: " + velocity.magnitude());
     }
 }
